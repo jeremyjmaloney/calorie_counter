7e70802297aaa13a20c910b79d53041c99909fc4
Thad Stinson <bootytee@Thads-iMac.local>
2019-05-15 18:06:42 -0500

    working index and create routes - tested with postman and in browser

--- controllers/mealController.js
+++ controllers/mealController.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const meals = express.Router()
+const express = require('express');
+const meals = express.Router();
+const Meals = require('../models/meal.js');
 
 
+// meals.get('/', (req, res) => {
+//   res.send('yo')
+// });
 
+// INDEX
+meals.get('/', async (req, res) => {
+  try {
+    const meals = await Meals.find()
+    res.status(200).json(meals)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+});
 
-meals.get('/', (req, res) => {
-  res.send('yo')
-})
+// CREATE
+meals.post('/', async (req, res) => {
+  try {
+    const newMeal = await Meals.create(req.body)
+    res.status(200).json(newMeal)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+});
+
 
 module.exports = meals
