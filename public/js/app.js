fe61c05e7cce3b21b773005777c5e753f9d49106
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-05-15 20:37:51 -0400

    Get meals, and create meals on index.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,37 @@
 const app = angular.module('CalorieApp', []);
 
 app.controller('MainController', ['$http', function($http){
-  this.test = 'yo';
+  let allMeals = [];
+
+  this.getMeals = function(){
+    $http({
+      method: 'GET',
+      url: '/meals'
+    }).then(response=>{
+      console.log(response.data);
+      this.mealsInfo = response.data;
+    }).catch(error=>{
+      console.log(error);
+    });
+  };
+
+  this.createMeal = function(){
+    $http({
+      method: 'POST',
+      url: '/meals',
+      data: {
+        name: this.name,
+        calories: this.calories
+      }
+    }).then(response=>{
+      console.log(response.data);
+      allMeals.push(response.data);
+      console.log(allMeals);
+    }).catch(error=>{
+      console.log(error);
+    });
+  };
+
+  this.getMeals();
+
 }]);
